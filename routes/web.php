b02d934ce6914a269560e582d9cd57d1e9105eb6
omer pervaiz <omerpervaiz@live.com>
2022-03-06 17:09:05 +0500

    updates

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,31 @@ Route::get('/', function () {
         date_default_timezone_set($timezone);
     }
 
+    if($country == ''){
+        $user_ip = get_client_ip();
+        $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+        $country = ($geo) ? $geo['geoplugin_countryName'] : '';
+    }
+
+    function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
+
     $tasks = Tasks::where(['type' => 'public'])->orWhere('user_country', $country)->get();
     return view('welcome', ['tasks' => $tasks, 'session_country' => $country]);
 });
